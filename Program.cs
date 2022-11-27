2fbcd1cc0c2230fbc795726b3dc7502ffebefaf9
riz-j <adiputra979@gmail.com>
2022-11-27 20:35:07 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello! What's your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Your name is {input}");
+        }
+    }
+}
